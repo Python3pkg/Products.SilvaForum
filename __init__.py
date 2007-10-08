15aeb6dff1210784097684a04429b5e4efb3566b
Todd Matsumoto <info@infrae.com>
2007-10-08 14:45:14 +0000

    Cleaned up a bit

--- __init__.py
+++ __init__.py
@@ -9,6 +9,5 @@ from Products.Silva.fssite import registerDirectory
 def initialize(context):
     from Products.Silva.fssite import registerDirectory
     registerDirectory('resources', globals())
-    #registerDirectory('emoticons/smilies', globals())
     path = os.path.join('emoticons', 'smilies')
     registerDirectory(path, globals())
