8e0ae4085ffe51efcb55c41e1c66bc3562b85398
Sylvain Viollon <thefunny@gmail.com>
2009-09-25 15:10:47 +0000

    Add some functional tests.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
 # Copyright (c) 2007-2008 Infrae. All rights reserved.
 # See also LICENSES.txt
-# SilvaForum
-# Silva
-
+# $Id$
 
 def initialize(context):
     from Products.Silva.fssite import registerDirectory
