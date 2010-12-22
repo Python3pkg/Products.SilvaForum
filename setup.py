9bf2774a9d6829744806767cf6d5279743b3c324
Sylvain Viollon <thefunny@gmail.com>
2010-12-22 12:09:33 +0100

    Tag 1.1.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1dev'
+version = '1.1'
 
 def product_path(filename):
     return os.path.join("Products", "SilvaForum", filename)
